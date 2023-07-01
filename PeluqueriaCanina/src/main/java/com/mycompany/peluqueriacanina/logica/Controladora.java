a1f187be7800990bc7c594745f47063770458268
cris90 <cristiantorrico90@gmail.com>
2023-07-01 13:10:31 -0300

    creamos las persistencia, controladores, enlazamos a la base de datos

--- PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/logica/Controladora.java
+++ PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/logica/Controladora.java
@@ -1,5 +1,31 @@
 package com.mycompany.peluqueriacanina.logica;
 
+import com.mycompany.peluqueriacanina.persistencia.ControladoraPersistencia;
+
 public class Controladora {
-    
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+
+    public void guardar(String nombreMasco, String raza, String color, 
+            String observaciones, String alergico, String atenEsp, 
+            String nomDuenio, String celDuenio) {
+
+        
+        Duenio duenio = new Duenio();
+        duenio.setNombre(nomDuenio);
+        duenio.setCelDuenio(celDuenio);
+        
+        
+        
+        Mascota masco = new Mascota();
+        masco.setNombre(nombreMasco);
+        masco.setColor(color);
+        masco.setRaza(raza);
+        masco.setAlergico(alergico);
+        masco.setAtencion_especial(atenEsp);
+        masco.setObservaciones(observaciones);
+        masco.setUnduenio(duenio);
+        
+        controlPersis.guardar(duenio, masco);
+        
+    }
 }
--- PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/persistencia/ControladoraPersistencia.java
+++ PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/persistencia/ControladoraPersistencia.java
@@ -1,6 +1,21 @@
 package com.mycompany.peluqueriacanina.persistencia;
 
+import com.mycompany.peluqueriacanina.logica.Duenio;
+import com.mycompany.peluqueriacanina.logica.Mascota;
+
 
 public class ControladoraPersistencia {
+    DuenioJpaController duenioJpa = new DuenioJpaController();
+    MascotaJpaController mascoJpa = new MascotaJpaController();
+
+    public void guardar(Duenio duenio, Mascota masco) {
+        
+        duenioJpa.create(duenio);
+        
+        
+        mascoJpa.create(masco);
+        
+        
+    }
     
 }
